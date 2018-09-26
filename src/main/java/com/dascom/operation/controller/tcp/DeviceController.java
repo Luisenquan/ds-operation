ee0f35b9619e49d96692090ca84a0a94f04c5b0f
雷森泉 <522267533@qq.com>
2018-09-26 15:57:33 +0800

    ds-operation 分支提交

--- src/main/java/com/dascom/operation/controller/tcp/DeviceController.java
+++ src/main/java/com/dascom/operation/controller/tcp/DeviceController.java
@@ -2,9 +2,9 @@ package com.dascom.operation.controller.tcp;
 
 
 
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,13 +24,13 @@ public class DeviceController {
 	private CollectionPrintersService printersService;
 	
 	@RequestMapping("readDevice/{number}")
-	public ResultVO read(@PathParam(value = "number") String number){
+	public ResultVO read(@PathVariable(value = "number") String number){
 		//判断设备是否存在
 		CollectionPrinters printer = printersService.fetchByNumber(number);
-		//return ResultVOUtil.success(printer);
+		return ResultVOUtil.success(printer);
 		//判断设备是否被占用
 		//读取设备信息
-		return deviceService.readDevice(number);
+		//return deviceService.readDevice(number);
 	}
 
 }
